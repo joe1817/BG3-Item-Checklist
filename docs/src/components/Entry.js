be32735e92a237b093e96c33733f5066ee0b4cc2
Joe James <1817@duck.com>
2025-05-07 12:30:29 -0400

    more props for more flexible components, however search may be somewhat slower

--- docs/src/components/Entry.js
+++ docs/src/components/Entry.js
@@ -1,19 +1,55 @@
 const Entry = {
-	props: ["data"],
 	template: `
 <div
 	ref="content"
-	:class="['entry', data.categories, data.rarity, {'top-tier': data.suggested, completed: $store.state.checkboxState[data.id], 'hide-image': !$store.state.showImagesState}]"
 	:id="data.id"
+	:class="['entry', data.categories, data.rarity, {'top-tier': data.suggested, completed: $store.state.checkboxState[data.id], 'hide-image': !$store.state.showImagesState}]"
 	v-show="visible"
 	@click="handleClick($event)"
 >
-	<span class="checkbox"><input type="checkbox" :checked="$store.state.checkboxState[data.id]"></span>
-	<span class="img"><img :src="data.img" width=50 height=50></span>
-	<span class="title"><a :href="data.link" v-html="$highlight(data.title)"></a></span>
-	<span class="desc" v-html="$highlight(data.desc)"></span>
+	<span v-if="checkbox" class="checkbox"><input type="checkbox" :checked="$store.state.checkboxState[data.id]"></span>
+	<span v-if="data.img" class="img"><img :src="data.img" width=50 height=50></span>
+
+	<template v-if="data.title">
+		<template v-if="data.link">
+			<span v-if="searchable" class="title"><a :href="data.link" v-html="$highlight(data.title)"></a></span>
+			<span v-else class="title"><a :href="data.link" v-html="data.title"></a></span>
+		</template>
+
+		<template v-else>
+			<span v-if="searchable" class="title" v-html="$highlight(data.title)"></span>
+			<span v-else class="title" v-html="data.title"></span>
+		</template>
+	</template>
+
+	<template v-if="data.desc">
+		<span v-if="searchable" class="desc" v-html="$highlight(data.desc)"></span>
+		<span v-else class="desc" v-html="data.desc"></span>
+	</template>
 </div>
 `,
+	props: {
+		data: {},
+		checkbox: {
+			type: Boolean,
+			default: true
+		},
+		searchable: {
+			type: Boolean,
+			default: true
+		}
+	},
+	data() {
+		const searchableArr = [];
+		if (this.data.title)
+			searchableArr.push("title");
+		if (this.data.desc)
+			searchableArr.push("desc");
+
+		return {
+			searchableArr: searchableArr,
+		};
+	},
 	mounted() {
 		if (this.progress) {
 			this.updateProgress(1);
@@ -24,7 +60,7 @@ const Entry = {
 	},
 	methods: {
 		handleClick(event) {
-			if (event.target.tagName !== "A") {
+			if (this.checkbox && event.target.tagName !== "A") {
 				this.$store.dispatch("toggleCheckboxAndSave", this.data.id)
 			}
 		},
@@ -45,23 +81,33 @@ const Entry = {
 	},
 	computed: {
 		visible() {
-			if (!this.$store.state.matchesSearch[this.data.parent.id] && this.$store.state.searchString.length && !this.data.desc.toLowerCase().includes(this.$store.state.searchString) && !this.data.title.toLowerCase().includes(this.$store.state.searchString)) {
+			if (this.searchable && !this.$store.state.matchesSearch[this.data.parent.id] && this.$store.state.searchString.length && !this.searchableArr.some(s => this.data[s].toLowerCase().includes(this.$store.state.searchString))) {
 				return false;
 			}
 			if (this.$store.state.checkboxState[this.data.id] && !this.$store.state.showCompleteState) {
 				return false;
 			}
-			for (const cat of this.data.categories) {
-				if (this.$store.state.filterState[cat] === true) {
-					return true;
+			if (this.data.categories !== undefined) {
+				for (const cat of this.data.categories) {
+					if (this.$store.state.filterState[cat] === true) {
+						return true;
+					}
 				}
+				return false;
+			} else {
+				return true;
 			}
-			return false;
 		},
 		progress() {
+			if (!this.checkbox) {
+				return 0;
+			}
 			return (this.visible && this.$store.state.checkboxState[this.data.id]) ? 1 : 0;
 		},
 		total() {
+			if (!this.checkbox) {
+				return 0;
+			}
 			return (this.visible) ? 1 : 0;
 		}
 	},
